b3a32b075f8f89ff5c482b4a242c37a5d92d5d0c
Sakshi R <rsakshi205@gmail.com>
2024-10-25 15:19:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+const febHolidays = [
+    "Hi my fav❤️",
+    "First of all, I like you❤️",
+    "You're my comfort zone",
+    "You are my source of happiness",
+    "And of course...",
+    "My fav person🤭",
+    "I will always be there for you",
+    "No matter how far we are🥰",
+    "You're the prettiest, cutest, funniest,",
+    "sweetest girl alive.",
+    "In one word, you're just perfect.😍",
+    "I'm the happiest man alive🥰",
+    "I'm so grateful that I met you❤️",
+    "I'll always do my best to keep you happy🥺",
+    "You are the most special person in my life🥰",
+    "I don't want to lose you",
+    "And trust me I dont want anyone else",
+    "There's no one better than you❤️",
+    "You're the best buddy!!",
+    "Or should I say, I hate u❤️",
+    "Wow!! Time flies, we've been fighting each other",
+    "Don't worry ella sari agutte nam madhya",
+    "but we overcame most of them",
+    "and it made me realised how important you are in my life❤️",
+    "This for you,",
+    "Sorry for all my childish nonsense things",
+    "and keep correcting me as always",
+    "and I'll keep irritating u🤣❤️",
+    "But anyways, I'm missing u a lot ",
+    "tension madkobedi, together we can solve any problem",
+    "I Love You So much❤️"
+  ];
+  const ulEl = document.querySelector("ul");
+  const d = new Date();
+  let daynumber = d.getMonth() == 1 ? d.getDate() - 1 : 0;
+  let activeIndex = daynumber;
+  const rotate = -360 / febHolidays.length;
+  init();
+  function init() {
+    febHolidays.forEach((holiday, idx) => {
+      const liEl = document.createElement("li");
+      liEl.style.setProperty("--day_idx", idx);
+      liEl.innerHTML = `<time datetime="2022-02-${idx + 1}">${
+        idx + 1
+      }</time><span>${holiday}</span>`;
+      ulEl.append(liEl);
+    });
+    ulEl.style.setProperty("--rotateDegrees", rotate);
+    adjustDay(0);
+  }
+  function adjustDay(nr) {
+    daynumber += nr;
+    ulEl.style.setProperty("--currentDay", daynumber);
+    const activeEl = document.querySelector("li.active");
+    if (activeEl) activeEl.classList.remove("active");
+    activeIndex = (activeIndex + nr + febHolidays.length) % febHolidays.length;
+    const newActiveEl = document.querySelector(
+      `li:nth-child(${activeIndex + 1})`
+    );
+    document.body.style.backgroundColor = window.getComputedStyle(
+      newActiveEl
+    ).backgroundColor;
+    newActiveEl.classList.add("active");
+  }
+  window.addEventListener("keydown", (e) => {
+    switch (e.key) {
+      case "ArrowUp":
+        adjustDay(-1);
+        break;
+      case "ArrowDown":
+        adjustDay(1);
+        break;
+      default:
+        return;
+    }
+  });
+  
